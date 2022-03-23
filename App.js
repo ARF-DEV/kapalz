3c1ada0ae5a236138e38578d4c83d3e4861ff470
PopoSnek <ariefuddinsatriadharma@gmail.com>
2022-03-23 23:00:17 +0700

    refactor : removing some import lines

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Input } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from './src/screen/Home'
